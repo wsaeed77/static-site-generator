dbbeae9c2cbddb83ef77f0bba90a38e4331bf907
Waqas Saeed <w.saeed77@gmail.com>
2020-06-15 12:00:00 +0100

    Add support for generating multiple pages from markdown

--- src/generator.py
+++ src/generator.py
@@ -6,13 +6,16 @@ from jinja2 import Template
 with open('templates/base.html') as f:
     template = Template(f.read())
 
-# Read the markdown content
-with open('content/index.md') as f:
-    content = markdown.markdown(f.read())
+# Process each markdown file in the content directory
+for markdown_file in os.listdir('content'):
+    with open(f'content/{markdown_file}') as f:
+        content = markdown.markdown(f.read())
 
-# Generate the final HTML
-output = template.render(title="Home", content=content)
+    # Generate the final HTML
+    title = markdown_file.replace('.md', '').capitalize()
+    output = template.render(title=title, content=content)
 
-# Write the output to a file
-with open('output/index.html', 'w') as f:
-    f.write(output)
+    # Write the output to a file
+    output_file = f'output/{markdown_file.replace(".md", ".html")}'
+    with open(output_file, 'w') as f:
+        f.write(output)
